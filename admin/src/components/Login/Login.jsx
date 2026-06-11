4a053034d269e6b2ec880cbd24e076d5cfcf65dc
Sabir Ali <128282383+sabir123ali@users.noreply.github.com>
2026-06-11 13:24:05 +0530

    Update Login.jsx

--- admin/src/components/Login/Login.jsx
+++ admin/src/components/Login/Login.jsx
@@ -20,7 +20,7 @@ const Login = ({ url }) => {
   };
   const onLogin = async (event) => {
     event.preventDefault();
-    const response = await axios.post(url + "/api/user/login", data);
+    const response = await axios.post("https://foodvilla-f0ef.onrender.com/api/user/login", data);
     console.log(response.data)
     if (response.data.success) {
       if (response.data.role === "admin") {
